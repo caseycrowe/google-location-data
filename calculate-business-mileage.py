f40ab67a8444ba7b4053a1c7f08ef2236b1e0ee1
caseycrowe <casey.crowe@gmail.com>
2020-07-12 19:17:19 -0400

    Update calculate-business-mileage.py
    Parameterized address, distance, and changed the output to be the name of the address searched.  ex: 123 Yourstreet.csv

--- calculate-business-mileage.py
+++ calculate-business-mileage.py
@@ -1,8 +1,3 @@
-# Script to count how many times a person went to a specific address
-# and on which date. Data is from Google Takeout. Export "Location History"
-# and drop this script in the folder with the YYYY_MONTH.json files and
-# run. It will prompt you for address and miles from home to location.
-
 import json
 import datetime
 import re
@@ -10,8 +5,9 @@ import csv
 import os
 import pandas as pd
 
-address = input("Enter address to search: ")
-miles = int(input("Enter distance in miles to address: "))
+# location = "placeVisit"
+address = input("Address to search: ")
+miles = int(float(input("Distance in miles to address: ")))
 
 
 print(f"Checking trips to '{address}'...")
@@ -24,14 +20,15 @@ for filename in os.listdir('.'):
 			# Convert the JSON to a Python dictionary:
 			objects = json_data['timelineObjects']
 			
-			# Write the data to a CSV 'locations.csv'
-			with open('locations.csv', mode='a', newline='') as locations_file:
+			# Write the data to a CSV '{address}.csv'
+			addressfile = address + '.csv'
+			with open(addressfile, mode='a', newline='') as locations_file:
 				article_writer = csv.writer(locations_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 				for places in objects:
 					    if 'placeVisit' in places:
 					        if 'name' in places['placeVisit']['location']:
-					        	if address in places['placeVisit']['location']['name']:
-						            location = places['placeVisit']['location']['name']
+					        	if address in places['placeVisit']['location']['address']:
+						            location = places['placeVisit']['location']['address'].replace("\n",", ")
 						            location = re.sub('[^\x00-\x7F]+', '', location)
 						            ms = int(places['placeVisit']['duration']['startTimestampMs'])
 						            stamp = datetime.datetime.fromtimestamp(ms/1000)
@@ -40,7 +37,7 @@ for filename in os.listdir('.'):
 						            article_writer.writerow([cleandate, location])
 
 # Clean up the resulting spreadsheet
-df = pd.read_csv('locations.csv',encoding = "ISO-8859-1")
+df = pd.read_csv(addressfile,encoding = "ISO-8859-1")
 rawtrips = len(df)
 print(f"Found {rawtrips} visits to {address}...")
 print("Sorting dates and removing duplicate trips to location...")
@@ -49,11 +46,9 @@ df = df.sort_values(by=['Date'])
 df.drop_duplicates(subset="Date",inplace=True)
 trips = len(df)
 print(f"Found {trips} unique trips to {address}...")
-print("Saving to file: locations.csv")
-df.to_csv('locations.csv', index=False)
+print(f"Saving to file: {addressfile}")
+df.to_csv(addressfile, index=False)
 print("Summary:")
 # 2 trips per visit (to/from home, {miles} miles per trip, $.58 per mile)
 writeoff = trips * 2 * miles * .58
 print(f"{trips} trips to '{address}' from home, at {miles} miles per trip, and $.58 per mile = ${writeoff}.")
-
-
